ddee64d109535022abd625aeaeddae8311ad6fe7
Sigalsha <sigal.shalit.manor@gmail.com>
2020-10-26 17:08:50 +0200

    first commit, basic structure, WIP

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button className="button" type="button">
+      Start Play
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// case start game => Welcome to Word Play!
+// case choose a subject => Select A Subject
+//
+
+const Header = () => {
+  return <h1 className="app-header">Welcome to Word Play!</h1>;
+};
+
+export default Header;
