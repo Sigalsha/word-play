6a94a840c1b1060ad66b492477e61fb2fcf4ae61
Sigalsha <sigal.shalit.manor@gmail.com>
2020-11-02 13:13:37 +0200

    adding images

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,11 @@
 import pizza from "../assets/food/pizza-slice.png";
 import computer from "../assets/computer/pc.png";
 import building from "../assets/buildings/congress.png";
+import clothes from "../assets/clothes/clothes.png";
+import animals from "../assets/animals/bear-face.png";
+import personal from "../assets/personal/heart-necklace.png";
+import household from "../assets/household/wooden-chair.png";
+import body from "../assets/body/strong.png";
 
 export const intro = {
   header: "Welcome to Word Play!",
@@ -11,13 +16,13 @@ export const intro = {
 export const selectSubject = {
   header: "Select a subject",
   subjects: [
-    { name: "animals", id: 1 },
-    { name: "clothes", id: 2 },
+    { name: "animals", id: 1, img: animals },
+    { name: "clothes", id: 2, img: clothes },
     { name: "food", id: 3, img: pizza },
     { name: "computer", id: 4, img: computer },
-    { name: "personal", id: 5 },
-    { name: "bathroom", id: 6 },
+    { name: "personal", id: 5, img: personal },
+    { name: "household", id: 6, img: household },
     { name: "buildings", id: 7, img: building },
-    { name: "places", id: 8 },
+    { name: "body", id: 8, img: body },
   ],
 };
