e40fe7a66010b213f38c331459d8c082bbfa40b3
Sigalsha <sigal.shalit.manor@gmail.com>
2022-03-21 14:33:04 +0200

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-# word-play
-
-English vocabulary game based on React Hooks and Styled Components
-
-## how it works?
-
-Choose a desired subject and practice your English vocabulary.
-
-## please visit: https://sigalsha.github.io/word-play/
--- src/components/Confetti.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useWindowSize } from "react-use";
-import Confetti from "react-confetti";
-
-export default () => {
-  const { width, height } = useWindowSize();
-  return <Confetti width={width} height={height} gravity={0.3} friction={1} />;
-};
